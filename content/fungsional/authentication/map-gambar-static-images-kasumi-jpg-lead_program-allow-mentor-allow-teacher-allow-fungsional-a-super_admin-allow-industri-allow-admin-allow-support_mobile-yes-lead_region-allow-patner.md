52b54f6ac62ca2e62fa51153d9097d37bf6a2180
Fuji Halim Rabbani <racun1601@gmail.com>
2023-08-16 13:20:48 +0700

    fix: config.yml and  gatsby-config.ts

---
+++ content/fungsional/authentication/map-gambar-static-images-kasumi-jpg-lead_program-allow-mentor-allow-teacher-allow-fungsional-a-super_admin-allow-industri-allow-admin-allow-support_mobile-yes-lead_region-allow-patner.md
@@ -0,0 +1,15 @@
+---
+fungsional: Login with email & password
+gambar: https://drive.google.com/uc?id=1ha32681fDH2GxJtUxMQUYw6UWsZI-uVb
+super_admin: Allow
+admin: Allow
+mentor: Allow
+teacher: Pending
+patner: Allow
+lead_program: Allow
+lead_region: Allow
+content_writer: Allow
+industri: Pending
+student: Allow
+support_mobile: Yes
+---
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,150 +1,57 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+interface EntryNode {
+  node: {
+    id: string;
+    frontmatter: {
+      gambar: string; // Change the type to string
+    };
+  };
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+interface QueryData {
+  allMarkdownRemark: {
+    edges: EntryNode[];
+  };
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+const IndexPage: React.FC<{ data: QueryData }> = ({ data }) => (
+  <div className="container mx-auto py-10">
+    <h1 className="text-3xl font-semibold mb-6">Netlify CMS and Gatsby</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div className="border rounded-lg overflow-hidden shadow-lg" key={node.id}>
+          <div className="relative">
+            {/* Use the gambar field directly as a string */}
+            {node.frontmatter.gambar && (
+              <img
+                src={node.frontmatter.gambar}
+                alt="Image Alt Text"
+                className="w-full h-48 object-cover"
+              />
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            gambar # Use the field directly as a string
+          }
+        }
+      }
+    }
   }
-]
+`;
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage: React.FC<PageProps> = () => {
-  return (
-   <>
-   <h1 className="text-center" >Hello World</h1>
-    <button className="bg-slate-900 text-center" >Click saya</button>
-   </>
-  )
-}
-
-export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
+export default IndexPage;
