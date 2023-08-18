d1f6a82fa250d2c178f217ca16c36dab653555ec
Fdvky1 <fdvkey@gmail.com>
2023-08-18 18:10:18 +0700

    feat: create index page and set the first of documentation as index view

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,17 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  result.data.dirs.edges.forEach(edge => {
+  
+  result.data.dirs.edges.forEach((edge, index) => {
+    if(index == 0){
+      actions.createPage({
+        path: `/`,
+        component: path.resolve('src/templates/docs.tsx'),
+        context: {
+          category: `/(${edge.node.name})/`
+        }
+      })
+    }
     actions.createPage({
       path: `/${edge.node.name}`,
       component: path.resolve('src/templates/docs.tsx'),
