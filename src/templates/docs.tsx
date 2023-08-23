8619c2479ba802e412cdf4a198bd52d69f67c226
Fuji Halim Rabbani <racun1601@gmail.com>
2023-08-23 15:06:49 +0700

    feat: back to top button

--- src/templates/docs.tsx
+++ src/templates/docs.tsx
@@ -1,4 +1,5 @@
-import * as React from "react"
+import * as React from "react";
+import { useEffect } from "react";
 import type { HeadFC } from "gatsby"
 import { Link, graphql } from "gatsby"
 import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image"
@@ -52,9 +53,53 @@ interface NestedDir {
 const IndexPage: React.FC<{ data: Data, pageContext: { pageName: string }}> = ({data, pageContext}) => {
   const [role, setRole] = React.useState<string | keyof FrontMatter>("all");
   const [imageModal, setImageModal] = React.useState<IGatsbyImageData | null>();
+  const [showBackToTop, setShowBackToTop] = React.useState(false);
+
+  const handleScroll = () => {
+    setShowBackToTop(window.scrollY > 300);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    if (showBackToTop) {
+      timeoutId = setTimeout(() => {
+        setShowBackToTop(false);
+      }, 3000);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [showBackToTop]);
   const filtered = data.docs.edges.filter(edge => role == "all" ? true : edge.node.frontmatter[role as keyof FrontMatter] == "Allow")
   return (
     <main style={pageStyles} className="bg-gray-100 dark:bg-gray-900 min-h-screen w-full">
+       <button
+        className={`${
+          showBackToTop ? "animate-fadeInUp" : "animate-fadeOutDown"
+        } fixed bottom-6 right-6 z-10 bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-md shadow-md`}
+        onClick={scrollToTop}
+      >
+       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18"/></svg>
+
+      </button>
       {imageModal == null ? <></> : (
         <div className="pt-16 fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full bg-gray-500/[0.8] dark:bg-gray-700/[0.8]">
           <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white absolute right-0 mr-8 -mt-12" onClick={() => setImageModal(null)}>
@@ -71,12 +116,12 @@ const IndexPage: React.FC<{ data: Data, pageContext: { pageName: string }}> = ({
       <Sidebar data={data.all} state={[role, setRole]} />
       <div className="container sm:ml-64 mr-auto w-auto px-11 pt-16 pb-8">
         <div className="mt-8 mb-3 text-gray-700 dark:text-gray-300">
-          <h1 className="font-semibold text-3xl capitalize mb-1">{pageContext.pageName}</h1>
+          <h1 className="font-semibold text-4xl capitalize mb-1 ">{pageContext.pageName}</h1>
             <ul className="max-w-md gap-1 text-gray-500 list-none dark:text-gray-400">
             {data.docs.edges.filter(edge => role == "all" ? true : edge.node.frontmatter[role as keyof FrontMatter] == "Allow").map(edge => {
               return (
                 <li key={edge.node.id}>
-                  <Link to={"#" + edge.node.frontmatter.fungsional} className="hover:text-gray-700 hover:dark:text-gray-300"><span className="text-sky-400" >#</span>{edge.node.frontmatter.fungsional} </Link>
+                  <Link to={"#" + edge.node.frontmatter.fungsional} className="hover:text-gray-700 text-xl hover:dark:text-gray-300"><span className="text-indigo-400 " >#</span>{edge.node.frontmatter.fungsional} </Link>
                 </li>
               )
             })}
