2edf97837c0e30b9296213f591b177ef9d632210
Fdvky1 <fdvkey@gmail.com>
2023-08-25 17:41:10 +0700

    fix: empty image cant build

--- src/templates/docs.tsx
+++ src/templates/docs.tsx
@@ -14,7 +14,7 @@ interface FrontMatter {
   fungsional: string;
   gambar: Array<{
     childImageSharp: any
-  }>;
+  }> | [];
   super_admin: string;
   admin: string;
   mentor: string;
@@ -103,9 +103,14 @@ const IndexPage: React.FC<{ data: Data, pageContext: { pageName: string }}> = ({
                   <h1 className="dark:text-slate-200 font-semibold mb-1 text-2xl sm:text-3xl"><span className="text-indigo-500 dark:text-indigo-400 mr-[0.05rem]" >#</span>{frontmatter.fungsional}</h1>
                   <span className={`text-slate-200 text-sm rounded-lg px-1.5 py-[0.075] ${frontmatter.support_mobile == "Yes" ? "bg-blue-500" : "bg-red-500"}`}>{frontmatter.support_mobile == "Yes" ? "Support Mobile" : "Not Support Mobile"}</span>
                   <article className="w-full dark:text-slate-300 font-light prose lg:prose-xl dark:prose-invert mt-3 mb-1" dangerouslySetInnerHTML={{ __html: edge.node.html}}></article>
-                  <div className="mx-3">
-                    <Gallery images={images} colWidth={90}/>
-                  </div>
+                  {images.length > 0 ? (
+                    <>
+                      <span className="dark:text-slate-200 font-semibold">Screenshot</span>
+                      <div className="mx-3">
+                        <Gallery images={images} colWidth={90}/>
+                      </div>
+                    </>
+                  ) : <></>}
                 </div>
               </li>
             )
