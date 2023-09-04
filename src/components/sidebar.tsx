7f7d43913ef1e37fdb56e4a640093026b0db44e7
Fdvky1 <fdvkey@gmail.com>
2023-09-04 10:19:14 +0700

    fix: sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,7 +8,7 @@ interface NestedDir {
   parents: string[];
 }
 
-const Sidebar: React.FC<{ data: Array<string>, state: [string, React.Dispatch<React.SetStateAction<string>>] }> = ({data, state}) => {
+const Sidebar: React.FC<{ data: Array<string>, pageName: string, state: [string, React.Dispatch<React.SetStateAction<string>>] }> = ({data, pageName, state}) => {
   const [role, setRole] = state;
   const [showSidebar, setSidebar] = React.useState<boolean>(false)
   const [showDropdown, setShowDropdown] = React.useState<string | null>(null);
@@ -20,8 +20,15 @@ const Sidebar: React.FC<{ data: Array<string>, state: [string, React.Dispatch<Re
         dir: menu.split("(")[0],
         parents: [...(typeof(dirs[i]) == 'string' ? [menu.split("(")[0]] : (dirs[i] as NestedDir).parents), menu]
       }
+    }else{
+      dirs.push({
+        dir: menu.split("(")[0],
+        parents: [menu]
+      })
     }
   }
+
+  console.log(dirs)
 
 
   return (
@@ -44,7 +51,7 @@ const Sidebar: React.FC<{ data: Array<string>, state: [string, React.Dispatch<Re
 
       <aside className={(showSidebar ? "" : "-translate-x-full") + " fixed top-0 left-0 w-[14.2rem] z-20 sm:w-64 h-screen transition-transform sm:translate-x-0"} aria-label="Sidebar">
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium pt-14">
+        <ul className="space-y-2 font-medium pt-14">
               <li className="mb-4">
               <button
                 type="button"
@@ -82,45 +89,39 @@ const Sidebar: React.FC<{ data: Array<string>, state: [string, React.Dispatch<Re
               </ul>
               </li>
               {dirs.map((dir, index) => {
-                const dropdownId = `dropdown-${index}`;
-                const userLocation = window.location.pathname.replace(/^\/+/, '');
-                const dirMatchesUserLocation = dir + '/' === userLocation;
+              const dropdownId = `dropdown-${index}`;
                 return (
-                  <li key={index} className="capitalize font-normal text-gray-700 dark:text-slate-300 transition-transform duration-200 ease-in-out transform hover:translate-x-[5px]">
-                      <span className={`w-3 h-3 rounded-full inline-block ${dirMatchesUserLocation ? 'bg-indigo-500' : ''}`}></span>
-                      { typeof dir == 'string' ? (
-                        <Link to={"/" + dir.replace(/(\w+)\((\w+)\)/g, "$1/$2")} className="p-3">{dir.replace(/\w+\((.*?)\)/g, "$1")}</Link>
+                  <li key={index} className="capitalize font-normal text-gray-700 dark:text-slate-300 transition-transform duration-200 ease-in-out transform hover:translate-x-[5px] ">
+                    { typeof dir == 'string' ? (
+                      <Link to={"/" + dir.replace(/(\w+)\((\w+)\)/g, "$1/$2")} className="flex items-center pl-4">{pageName == dir ? (
+                        <i className="w-2 h-2 mr-2 rounded-full inline-block bg-indigo-500 -ml-4"></i>
                       ) : (
-                        <>
-                      <button
-                        type="button"
-                        className="flex items-center w-full p-3 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        onClick={() => setShowDropdown(showDropdown === dropdownId ? null : dropdownId)}
-                      >
-                        <span className="flex-1 text-left whitespace-nowrap capitalize">
-                          {userLocation.startsWith(dir.dir) && (
-                            <span className={`w-3 h-3 rounded-full mr-3 inline-block bg-indigo-500`}></span>
-                          )}
-                          {dir.dir}
-                        </span>
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                        </svg>
-                      </button>
-                        <ul id={dropdownId} className={(showDropdown === dropdownId ? "" : "hidden") + " py-2 space-y-4 ml-3"}>
-                        {dir.parents.map((_dir, index) => {
-                          const dirUserName = _dir.replace(/(\w+)\((\w+)\)/g, "$1/$2")
-                          const dirUserLocation =  dirUserName + '/' === userLocation;
-                          return (
-                            <li key={dir.dir + index} >
-                              {dirUserLocation  && (
-                                <span className={`w-3 h-3 mr-3 rounded-full inline-block bg-indigo-500`}></span>
-                              )}
-                              <Link to={"/" + _dir.replace(/(\w+)\((\w+)\)/g, "$1/$2")}>{_dir.replace(/\w+\((.*?)\)/g, "$1")}</Link>
+                        <></>
+                      )}<span>{dir.replace(/\w+\((.*?)\)/g, "$1")}</span></Link>
+                    ) : (
+                      <>
+                        <button
+                          type="button"
+                          className={`ml-0.5 flex items-center w-full py-1.5 px-3 text-base transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 ${showDropdown === dropdownId ? "bg-gray-200 dark:bg-gray-600" : ""}`}
+                          onClick={() => setShowDropdown(dropdownId == showDropdown ? '' : dropdownId)}
+                          >
+                          <span className="flex-1 text-left whitespace-nowrap capitalize">{dir.dir}</span>
+                          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                          </svg>
+                        </button>
+                        <ul id={dropdownId} className={((showDropdown === dropdownId || pageName.startsWith(dir.dir)) ? "" : "hidden") + " py-2 space-y-2 ml-4"}>
+                          {dir.parents.map((_dir, index) => (
+                            <li key={dir.dir + index}>
+                              <Link to={"/" + _dir.replace(/(\w+)\((\w+)\)/g, "$1/$2")} className="flex items-center pl-4">
+                                {pageName == _dir ? (
+                                  <i className="w-2 h-2 mr-2 rounded-full inline-block bg-indigo-500 -ml-4"></i>
+                                ) : (
+                                  <></>
+                                )}<span>{_dir.replace(/\w+\((.*?)\)/g, "$1")}</span></Link>
                             </li>
-                          );
-                        })}
-                      </ul>
+                          ))}
+                        </ul>
                       </>
                     )}
                   </li>
